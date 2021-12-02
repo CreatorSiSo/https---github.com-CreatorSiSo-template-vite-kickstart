39b56c23f41db74a67233c2184995ab12986a840
CreatorSiSo <creatorsisohd@gmail.com>
2021-12-02 07:07:57 +0100

    Add html parsing.

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,8 @@
 import icoVite from '@icons/ico-vite.svg?raw';
-// import icoVite from '@/components/app/_sprite-items/icons/ico-vite.svg?raw';
 
 document.querySelector('#svg-sprite-storage').innerHTML = icoVite;
 
-document.querySelector('main').innerHTML = `
+document.querySelector('#content').innerHTML = `
   <h1>Hello from Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">
     Documentation
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,52 @@
 // https://vitejs.dev/config/
 
+import { Parser } from 'htmlparser2';
+import { DomHandler } from 'domhandler';
+import { getElements, getName, textContent } from 'domutils';
+import serializeDOM from 'dom-serializer';
+
+/**
+ * @type {import('vite').Plugin}
+ */
+const kickstart = {
+  name: 'kickstart',
+
+  transform(code, id) {
+    if (id.endsWith('.html')) console.log(code);
+    return code;
+  },
+
+  transformIndexHtml(html, _context, _bundle) {
+    let returnHtml;
+
+    const handler = new DomHandler(
+      (error, dom) => {
+        if (error) {
+          console.error('Could not parse DOM.');
+        } else {
+          getElements({ tag_name: 'HeaderComponent' }, dom, true).forEach(
+            (element) => console.log(textContent(element))
+          );
+
+          returnHtml = serializeDOM(dom, { xmlMode: false });
+        }
+      },
+      { withStartIndices: true, withEndIndices: true }
+    );
+
+    const parser = new Parser(handler, {
+      xmlMode: true,
+      recognizeSelfClosing: true,
+    });
+    parser.write(html);
+    parser.end();
+
+    return returnHtml;
+  },
+};
+
 import { defineConfig } from 'vite';
-import path from 'path';
+import { getElementsByTagName } from 'domutils';
 
 export default defineConfig({
   resolve: {
@@ -9,4 +54,5 @@ export default defineConfig({
       '@icons': '/components/app/_sprite-items/icons/',
     },
   },
+  plugins: [kickstart],
 });
